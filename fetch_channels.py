6d3ed409fcb90870483bf8149747e91a4a969ab8
iptvorg-bd <feniireen2@gmail.com>
2026-09-08 03:15:21 +0600

    Update fetch_channels.py
    655uy7u

--- fetch_channels.py
+++ fetch_channels.py
@@ -2,7 +2,7 @@ import requests
 import json
 import sys
 
-url = "https://api.tivihub.app/api/channels"
+url = "https://api.tivihub.app/matches.m3u"
 
 headers = {
     'accept': 'application/json',
